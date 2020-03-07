1e41865a5e787966847376e4cb85f757a8d9e571
dsf <mahmmad.alnajjar@ogr.sakarya.edu.tr>
2020-03-07 09:15:08 +0300

    hhhhhhh

---
+++ src/main.c
@@ -0,0 +1,146 @@
+/*******************************************************************************
+ * @file    main.c
+ * @author  Ahmed Eldeep
+ * @email   ahmed@almohandes.org
+ * @website http://almohandes.org/stm32
+ * @date    21.03.2018
+ *          
+ * @brief   main application called after startup
+ * @note    
+ *
+@verbatim
+Copyright (C) Almohandes.org, 2018
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <http://www.gnu.org/licenses/>.
+@endverbatim
+*******************************************************************************/
+
+/* Includes */
+#include "nvic.h"
+#include "SysTick.h"
+#include "gpio.h"
+#include "exti.h"
+
+/**
+ * @addtogroup stm32_examples
+ * @{
+ */
+
+/**
+ * @defgroup main
+ * @brief
+ * @{
+ */
+
+/**
+ * @defgroup main_private_typedefs
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup main_private_defines
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup main_private_macros
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup main_private_constants
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup main_private_variables
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup main_private_function_prototypes
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup main_private_functions
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup main_exported_functions
+ * @{
+ */
+
+/**
+ * @brief   Main function
+ * @note
+ * @param   none
+ * @retval  none
+ */
+int main(void)
+{
+  SysTick_Init();
+  GPIO_Init_LED(EVAL_ALL_LEDs);
+
+  GPIO_Init_PB();
+  EXTI_Init_PB();
+  NVIC_Init();
+
+  /* Clear PRIMASK, enable IRQs */
+  __enable_irq();
+
+  /* Infinite loop */
+  while(1)
+  {
+//    /* Trigger EXTI0 */
+//    EXTI->SWIER = EXTI_SWIER_SWIER0;
+  }
+}
+
+/**
+ * @}
+ */
+/**
+ * @}
+ */
+/**
+ * @}
+ */
